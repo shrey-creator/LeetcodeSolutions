59f51e4b519abca550c1dd43dddf7ee48d179440
shrey agrawal <60284082+shrey-creator@users.noreply.github.com>
2022-01-21 21:23:43 +0530

    Added solution - LeetHub

---
+++ 3. Count Total Digits in a Number - GFG/3.-count-total-digits-in-a-number.c
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int countDigits(int n)
+{
+    if((n/10)==0)
+    return 1;
+    return 1+countDigits(n/10);
+   //Your code here
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int T;
+	
+	//taking testcases
+	scanf("%d", &T);
+	while(T--)
+	{
+	    int n;
+	    
+	    //taking number n
+	    scanf("%d", &n);
+	    
+	    //calling countDigits
+	    printf("%d\n", countDigits(n));
+	    
+	    
+	}
+	return 0;
+}
+  // } Driver Code Ends
